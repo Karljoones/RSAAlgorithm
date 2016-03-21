0bfcad79b93f1a90d6407b8aea63ea0e23718d12
KarlJoones- <C13315991@mydit.ie>
2016-03-21 14:49:09 +0000

    Initial

---
+++ src/security/assignment/karl/GUI.java
@@ -0,0 +1,214 @@
+package security.assignment.karl;
+
+/**
+ * Created by Karl on 21/03/2016.
+ */
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigInteger;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    private final JTextField nTF = new JTextField(40); // set TextField for 40
+    private final JTextField publicKeyTF = new JTextField(40);
+    private final JTextField privateKeyTF = new JTextField(40);
+    private final JTextArea plainTextTA = new JTextArea();
+    private final JTextArea cipherTextTA = new JTextArea();
+    private final String[] types = { "Alphanumeric", "File" };
+    private final JComboBox<String> type = new JComboBox<String>(types);
+
+    public GUI() {
+        setSize(new Dimension(730, 400));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit the program when
+        setTitle("RSA");// set window title
+        addComponentsToPane(getContentPane()); // add components
+        setResizable(false); // window is not resizable
+        setVisible(true); // show window
+
+        // Set the frame to be centered
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2 - getSize().width/2, dim.height/2 - getSize().height/2);
+    }
+
+    private void addComponentsToPane(Container pane) {
+        pane.setLayout(new GridBagLayout()); // set Layout to pane
+        GridBagConstraints c = new GridBagConstraints(); // constraints for
+
+        JLabel nLabel = new JLabel("n:");// create label
+        c.insets = new Insets(5, 5, 5, 5); // set paddings
+        c.gridx = 0;// set x coordinate (horizontal)
+        c.gridy = 0;// set y coordinate (vertical)
+        pane.add(nLabel, c);// add to pane
+
+        c.gridx = 1;
+        c.gridy = 0;
+        pane.add(nTF, c);
+
+        JLabel publicKeyLabel = new JLabel("Public key:");
+        c.weightx = 0.1;
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(publicKeyLabel, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        pane.add(publicKeyTF, c);
+
+        JLabel privateKeyLabel = new JLabel("Private key:");
+        c.gridx = 0;
+        c.gridy = 2;
+        pane.add(privateKeyLabel, c);
+
+        c.gridx = 1;
+        c.gridy = 2;
+        pane.add(privateKeyTF, c);
+
+        JButton generateKeys = new JButton("Generate Keys"); // add "Generate
+        // Keys" button
+        generateKeys.addActionListener(new ActionListener() {
+            // when click on button
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BigInteger[] keys = RSA.generateKeys(); // generate public key,
+                // private key and n;
+                publicKeyTF.setText(keys[0].toString(16));// show public key as
+                // hex String
+                privateKeyTF.setText(keys[1].toString(16));// show private key
+                // as hex String
+                nTF.setText(keys[2].toString(16));// show n as hex String
+            }
+        });
+        c.gridx = 0;
+        c.gridy = 3;
+        pane.add(generateKeys, c);
+
+        c.gridx = 1;
+        c.gridy = 3;
+        pane.add(type, c);
+
+        JPanel textPanel = new JPanel(); // create panel to add there text areas
+        // with set size
+
+        JScrollPane plainTextSP = new JScrollPane(plainTextTA);// add text area to scroll pane
+        // for enabling scrolling
+        plainTextSP.setPreferredSize(new Dimension(300, 100));// set size of scroll pane
+        plainTextTA.setLineWrap(true);// set line wrap in text area
+        textPanel.add(plainTextSP);// add text area to panel
+
+        JScrollPane cipherTextSP = new JScrollPane(cipherTextTA);// add text area to scroll pane
+        // for enabling scrolling
+        cipherTextSP.setPreferredSize(new Dimension(300, 100));// set size of
+        // scroll pane
+        cipherTextTA.setLineWrap(true);
+        textPanel.add(cipherTextSP);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        c.gridwidth = 2; // will take 2 columns;
+        pane.add(textPanel, c);// add panel to pane
+
+        JPanel buttonPanel = new JPanel();// create panel to add there buttons
+        // with seted size
+
+        JButton encrypt = new JButton("Encrypt");// create button
+        encrypt.setPreferredSize(new Dimension(300, 30));// set button size
+        encrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (type.getSelectedItem().toString()) {
+                    case "Alphanumeric":
+					/*
+					 * If selected type of data is "message" get text from
+					 * plainTextTA text area, public key from publicKeyTF text
+					 * field, n from nTF text field, encrypt text and show
+					 * encrypted text in cipherTextTA text area as hex String
+					 */
+                        if(!plainTextTA.getText().equals("")){
+                            cipherTextTA.setText(RSA.encrypt(plainTextTA.getText(), publicKeyTF.getText(), nTF.getText()));
+                            plainTextTA.setText("");
+                        } else {
+                            alertUser("encrypt");
+                        }
+                        break;
+                    case "File":
+					/*
+					 * If selected type of data is "file" get file name from
+					 * plainTextTA text area, public key from publicKeyTF text
+					 * field, n from nTF text field, encrypt file and show
+					 * "Completed" in cipherTextTA text area if there is no
+					 * exception or error message, if there is some exceptions
+					 */
+                        if(!plainTextTA.getText().equals("")) {
+                            try {
+                                RSA.encryptFile(plainTextTA.getText(), publicKeyTF.getText(), nTF.getText());
+                                cipherTextTA.setText("Completed");
+                            } catch (Exception e1) {
+                                cipherTextTA.setText("No such file exists.");
+                            }
+                        } else {
+                            alertUser("encrypt");
+                        }
+                }
+            }
+        });
+        buttonPanel.add(encrypt);// add button to panel
+
+        JButton decrypt = new JButton("Decrypt");
+        decrypt.setPreferredSize(new Dimension(300, 30));
+        decrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (type.getSelectedItem().toString()) {
+                    case "Alphanumeric":
+					/*
+					 * If selected type of data is "message" get text from
+					 * cipherTextTA text area, private key from privateKeyTF
+					 * text field, n from nTF text field, decrypt it and show
+					 * plain text in plainTextTA text area
+					 */
+                        if(!cipherTextTA.getText().equals("")) {
+                            plainTextTA.setText(RSA.decrypt(cipherTextTA.getText(), privateKeyTF.getText(), nTF.getText()));
+                            cipherTextTA.setText("");
+                        } else {
+                            alertUser("decrypt");
+                        }
+                        break;
+                    case "File":
+					/*
+					 * If selected type of data is "file" get file name from
+					 * cipherTextTA text area, private key from privateKeyTF
+					 * text field, n from nTF text field, decrypt file and show
+					 * "Completed" in plainrTextTA text area if there is no
+					 * exception or error message, if there is some exceptions
+					 */
+                        if(!cipherTextTA.getText().equals("")) {
+                            try {
+                                RSA.decryptFile(cipherTextTA.getText(), privateKeyTF.getText(), nTF.getText());
+                                plainTextTA.setText("Completed");
+                            } catch (Exception e1) {
+                                plainTextTA.setText("No such file exists.");
+                            }
+                        } else {
+                            alertUser("decrypt");
+                        }
+                }
+            }
+        });
+        buttonPanel.add(decrypt);// add button to panel
+
+        c.gridx = 0;
+        c.gridy = 5;
+        c.gridwidth = 2;
+        pane.add(buttonPanel, c);// add button panel to pane
+    }
+
+    /**
+     * Alert the user when trying to decrypt of encrypt fields.
+     */
+    public void alertUser(String box){
+        JOptionPane.showMessageDialog(null, "Can not " + box.toLowerCase() + " when the field is empty.", "Attention!", JOptionPane.WARNING_MESSAGE);
+    }
+}
---
+++ src/security/assignment/karl/RSA.java
@@ -0,0 +1,216 @@
+package security.assignment.karl;
+
+/**
+ * Created by Karl on 21/03/2016.
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
+
+public class RSA {
+    private static int bitlen = 128; // bit length of numbers
+
+    /**
+     * Encrypt the given plaintext message.
+     */
+    public static String encrypt(String message, String e, String n) {
+        // Take out the spaces, non-alphanumeric characters and put the string in lower case
+        String newMessage = message.replaceAll("\\s+", "");
+        newMessage = newMessage.replaceAll("[^A-Za-z0-9]", "");
+        newMessage = newMessage.toLowerCase();
+        BigInteger m = new BigInteger(newMessage.getBytes()); // convert String to
+        // BigInteger for
+        // encryption
+        BigInteger eB = new BigInteger(e, 16);// parse hex String to BigInteger
+        BigInteger nB = new BigInteger(n, 16);// parse hex String to BigInteger
+        BigInteger tmp, res = BigInteger.ZERO;
+        int i = 0;
+        while (!m.equals(BigInteger.ZERO)) {
+			/* in this loop we encrypt every block
+			 get remainder of the division, encrypt it, and add to encrypted variable,
+			 then divide file variable into n and do the same thing, and add encrypted block
+			 multiplied by n^i, where i is index of iteration*/
+            tmp = m.mod(nB);
+            res = res.add(modular_pow(tmp, eB, nB).multiply(nB.pow(i)));
+            i++;
+            m = m.divide(nB);
+        }
+        return res.toString(16);// encrypting and return hex String
+    }
+
+    /**
+     * Decrypt the given ciphertext message.
+     */
+    public static String decrypt(String message, String d, String n) {
+        BigInteger m = new BigInteger(message, 16); // parse hex String to
+        // BigInteger
+        BigInteger dB = new BigInteger(d, 16);// parse hex String to BigInteger
+        BigInteger nB = new BigInteger(n, 16);// parse hex String to BigInteger
+        BigInteger tmp, res = BigInteger.ZERO;
+        int i = 0;
+        while (!m.equals(BigInteger.ZERO)) {
+			/* in this loop we encrypt every block
+			 get remainder of the division, encrypt it, and add to encrypted variable,
+			 then divide file variable into n and do the same thing, and add encrypted block
+			 multiplied by n^i, where i is index of iteration*/
+            tmp = m.mod(nB);
+            res = res.add(modular_pow(tmp, dB, nB).multiply(nB.pow(i)));
+            i++;
+            m = m.divide(nB);
+        }
+        return new String(res.toByteArray());// decrypt and
+    }
+
+    /**
+     * Generate the public and the private keys.
+     * @return BigInteger: keys
+     */
+    public static BigInteger[] generateKeys() {
+        BigInteger n, d, e; // security.assignment.karl.RSA parameters
+        SecureRandom r = new SecureRandom();
+        BigInteger p = new BigInteger(bitlen / 2, 100, r); // generate random p
+        int certainty = 1000;
+        while (!p.isProbablePrime(certainty)) {
+            p = new BigInteger(bitlen / 2, 100, r); // if number is not probable
+            // prime, generate new
+            // number (probability that
+            // number is prime is
+            // (1-1/2^certainty)
+        }
+        BigInteger q = new BigInteger(bitlen / 2, 100, r); // generate random
+        while (!q.isProbablePrime(certainty)) {
+            q = new BigInteger(bitlen / 2, 100, r);
+        }
+        n = p.multiply(q); // n=p*q
+        BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE)); // Euclidean
+        // algorithm;
+        e = new BigInteger("3");
+        while (gcd(m, e).intValue() > 1) { // find Coprime integer to m;
+            e = e.add(new BigInteger("2"));
+        }
+        d = mul_inv(e, m); // calculate private key
+        BigInteger[] out = { e, d, n };
+        return out; // return Array of public key, private key and n;
+    }
+
+    /**
+     * Extended euclidean algorithm.
+     */
+    private static BigInteger mul_inv(BigInteger a, BigInteger b) {
+
+        BigInteger b0 = b, tmp, q;
+        // we use q to remember quotient, tmp as temporary variable, b0 to
+        // remember initial value of b
+        BigInteger x0 = BigInteger.ZERO, x1 = BigInteger.ONE;
+        if (b.equals(BigInteger.ONE))
+            return BigInteger.ONE;
+        while (a.compareTo(BigInteger.ONE) > 0) {
+            q = a.divide(b);
+            tmp = b;
+            b = a.mod(b);
+            a = tmp;
+            tmp = x0;
+            x0 = x1.subtract(q.multiply(x0));
+            x1 = tmp;
+        }
+        if (x1.compareTo(BigInteger.ZERO) < 0)
+            x1 = x1.add(b0); // if result < 0, we can get it modulo ( (-a) mod n
+        // = n - a = (-a) + n)
+        return x1;
+    }
+
+    private static BigInteger modular_pow(BigInteger b, BigInteger e, BigInteger m) {
+        BigInteger x = new BigInteger("1"); // The default value of x
+        BigInteger power;
+        power = b.mod(m);
+        String t = e.toString(2); // convert the power to string of binary
+        String reverse = new StringBuffer(t).reverse().toString();
+        for (int i = 0; i < reverse.length(); i++) { // this loop to go over the
+            // string char by char
+            // by reverse
+            if (reverse.charAt(i) == '1') { // the start of if statement when
+                // the char is 1
+                x = x.multiply(power);
+                x = x.mod(m);
+            }
+            power = power.multiply(power);
+            power = power.mod(m);
+            // the end of if statement
+        } // the end of for loop
+        return x;
+    }
+
+    /**
+     * Encrypt the file that it is given.
+     * @param path : file path
+     */
+    public static BigInteger encryptFile(String path, String e, String n) throws IOException {
+        String fileName = path; // save file name
+        path = "plainFiles/" + path;// add directory name to the file path
+        BigInteger eB = new BigInteger(e, 16); // parse hex String to BigInteger
+        BigInteger nB = new BigInteger(n, 16);
+        Path filePath = Paths.get(path);// create path to file
+        BigInteger file = new BigInteger(Files.readAllBytes(filePath)); // generate BigInteger from
+        // byte array, which was
+        // got from file
+        BigInteger encrypted = BigInteger.ZERO;
+        BigInteger tmp;
+        int i = 0;
+        while (!file.equals(BigInteger.ZERO)) {
+			/* in this loop we encrypt every block of file
+			 get remainder of the division, encrypt it, and add to encrypted variable,
+			 then divide file variable into n and do the same thing, and add encrypted block
+			 multiplied by n^i, where i is index of iteration*/
+            tmp = file.mod(nB);
+            encrypted = encrypted.add(modular_pow(tmp, eB, nB).multiply(nB.pow(i)));
+            i++;
+            file = file.divide(nB);
+        }
+        // with the help of FileOutputStream generate new file with the same name
+        FileOutputStream fos = new FileOutputStream("encryptedFiles/" + fileName);
+        fos.write(encrypted.toByteArray());
+        fos.close();
+        return encrypted;
+    }
+
+    public static void decryptFile(String path, String d, String n) throws IOException {
+        String fileName = path;
+        path = "encryptedFiles/" + path;
+        BigInteger dB = new BigInteger(d, 16);// parse hex String to BigInteger
+        BigInteger nB = new BigInteger(n, 16);
+        Path filePath = Paths.get(path);
+        BigInteger file = new BigInteger(Files.readAllBytes(filePath));
+        BigInteger decrypted = BigInteger.ZERO;
+        BigInteger tmp;
+        int i = 0;
+        while (!file.equals(BigInteger.ZERO)) {
+            tmp = file.mod(nB);
+            decrypted = decrypted.add(modular_pow(tmp, dB, nB).multiply(nB.pow(i)));
+            i++;
+            file = file.divide(nB);
+        }
+        FileOutputStream fos = new FileOutputStream("plainFiles/" + fileName	);
+        fos.write(decrypted.toByteArray());
+        fos.close();
+    }
+
+    /**
+     * Get the greatest common divisor
+     */
+    public static BigInteger gcd(BigInteger a, BigInteger b) {
+        if (b.equals(BigInteger.ZERO))
+            return a;
+        else
+            return gcd(b, a.mod(b)); // when a divided into b, a mod b = 0, so a is gcd;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Initialise the security.assignment.karl.GUI.
+        new GUI();
+    }
+}
